b9afaa3173d8631fc627ddee4bce69ba958ac111
Gabriel Pérez <gph393@gmail.com>
2023-11-22 22:24:53 -0400

    Add Soft Skills and Alumni CRUD to the root module graph

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
+import { SoftSkillsModule } from './soft-skills/soft-skills.module';
+import { AlumniModule } from './alumni/alumni.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [SoftSkillsModule, AlumniModule],
 })
 export class AppModule {}
