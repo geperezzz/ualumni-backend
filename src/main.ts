461e6da59f8806157008d74380d7b1044c36b414
Gabriel Pérez <gph393@gmail.com>
2023-11-26 16:42:23 -0400

    Format code

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     new ValidationPipe({
       transform: true,
       whitelist: true,
-      forbidNonWhitelisted: true
+      forbidNonWhitelisted: true,
     }),
   );
   await app.listen(3000);
