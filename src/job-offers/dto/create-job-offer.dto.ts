1fd87bc20b704ad93553d3c52afd79184cc74c25
Gabriel Pérez <gph393@gmail.com>
2023-11-26 16:41:11 -0400

    Add job offers CRUD

---
+++ src/job-offers/dto/create-job-offer.dto.ts
@@ -0,0 +1,57 @@
+import { Type } from 'class-transformer';
+import {
+  IsBase64,
+  IsDate,
+  IsDefined,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+
+export class CreateJobOfferDto {
+  @IsUUID()
+  @IsOptional()
+  id?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  companyName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  companyContact: string;
+
+  @IsBase64()
+  @IsNotEmpty()
+  companyLogo: string;
+
+  @IsString()
+  @IsNotEmpty()
+  department: string;
+
+  @IsString()
+  @IsNotEmpty()
+  position: string;
+
+  @IsString()
+  @IsNotEmpty()
+  offerLocation: string;
+
+  @IsDate()
+  @Type(() => Date)
+  @IsDefined()
+  offerTimestamp: Date;
+
+  @IsString()
+  @IsDefined()
+  careerName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  contractTypeName: string;
+}
