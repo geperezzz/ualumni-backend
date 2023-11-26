2d078d0fb4fdcfb621c72f89d788b249bc587a0f
Gabriel Pérez <gph393@gmail.com>
2023-11-26 16:42:12 -0400

    Add job applications, job offers and graduations CRUDs to the module graph

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { SoftSkillsModule } from './soft-skills/soft-skills.module';
 import { AlumniModule } from './alumni/alumni.module';
+import { JobApplicationsModule } from './job-applications/job-applications.module';
+import { JobOffersModule } from './job-offers/job-offers.module';
+import { GraduationsModule } from './graduations/graduations.module';
 
 @Module({
-  imports: [SoftSkillsModule, AlumniModule],
+  imports: [
+    SoftSkillsModule,
+    AlumniModule,
+    JobApplicationsModule,
+    JobOffersModule,
+    GraduationsModule,
+  ],
 })
 export class AppModule {}
