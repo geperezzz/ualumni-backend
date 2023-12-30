3aabb5c9b7ed881555e7126d732e1461917fbd6d
GloodBuster <95661756+GloodBuster@users.noreply.github.com>
2023-12-30 16:23:06 -0400

    import UalumniDbModule to job offer technical skill module

--- src/job-offer-technical-skill/job-offer-technical-skill.module.ts
+++ src/job-offer-technical-skill/job-offer-technical-skill.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JobOfferTechnicalSkillService } from './job-offer-technical-skill.service';
 import { JobOfferTechnicalSkillController } from './job-offer-technical-skill.controller';
+import { UalumniDbModule } from 'src/ualumni-db/ualumni-db.module';
 
 @Module({
+  imports: [UalumniDbModule],
   controllers: [JobOfferTechnicalSkillController],
   providers: [JobOfferTechnicalSkillService],
 })
