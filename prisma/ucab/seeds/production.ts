fd05c4dfca21d754eb68657a38dce154c63c647b
Gabriel Pérez <gph393@gmail.com>
2024-01-04 21:35:31 -0400

    Remove the careers that are not available in Guayana

--- prisma/ucab/seeds/production.ts
+++ prisma/ucab/seeds/production.ts
@@ -15,38 +15,21 @@ async function seedCareers(transaction: Transaction): Promise<void> {
       { name: 'Administración de Empresas' },
       { name: 'Contaduría Pública' },
       { name: 'Relaciones Industriales' },
-      { name: 'Sociología' },
-      { name: 'Economía' },
-      { name: 'Economía' },
 
       // Facultad de Derecho
       { name: 'Derecho' },
 
       // Facultad de Humanidades y Educación
-      { name: 'Letras' },
-      { name: 'Psicología' },
-      { name: 'Filosofía' },
       { name: 'Comunicación Social' },
-      { name: 'Comunicación Social' },
-      { name: 'Educación: Mención Biología y Química' },
-      { name: 'Educación: Mención Ciencias Pedagógicas' },
       { name: 'Educación: Mención Ciencias Sociales' },
-      { name: 'Educación: Mención Filosofía' },
-      { name: 'Educación: Mención Física y Matemática' },
       { name: 'Educación: Mención Integral' },
       { name: 'Educación: Mención Inicial' },
-      { name: 'Educación: Mención Idiomas Modernos-Inglés' },
-      { name: 'Programa Especial de Licenciatura en Educación' },
+      { name: 'Educación: Mención Física y Matemática' },
 
       // Facultad de Ingeniería
-      { name: 'Ingeniería en Telecomunicaciones' },
       { name: 'Ingeniería Civil' },
       { name: 'Ingeniería Industrial' },
       { name: 'Ingeniería Informática' },
-      { name: 'Arquitectura' },
-
-      // Facultad de Teología
-      { name: 'Teología' },
     ],
   });
 }
